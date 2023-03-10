361cac326568cb05b5f101154671ed31020213fa
charise-arter-sony <113930495+charise-arter-sony@users.noreply.github.com>
2023-03-09 17:53:07 -0800

    initial setup, css enabled, react connected

--- app/electron/main.js
+++ app/electron/main.js
@@ -1,9 +1,11 @@
 const { app, BrowserWindow } = require('electron');
 
-app.whenReady().then(() => {
+function createWindow() {
+	// create new window
 	let window = new BrowserWindow({
 		width: 800,
 		height: 600,
+		show: false,
 	});
 
 	window.webContents.on('did-finish-load', () => {
@@ -11,5 +13,24 @@ app.whenReady().then(() => {
 		window.focus();
 	});
 
-	window.loadFile('index.html');
+	window.loadFile('app/dist/index.html');
+}
+
+app.whenReady().then(() => {
+	createWindow();
+
+	app.on('activate', () => {
+		if (BrowserWindow.getAllWindows().length === 0) {
+			createWindow();
+		}
+	});
+});
+
+// Quit when all windows are closed, except on macOS. There, it's common
+// for applications and their menu bar to stay active until the user quits
+// explicitly with Cmd + Q.
+app.on('window-all-closed', function () {
+	if (process.platform !== 'darwin') {
+		app.quit();
+	}
 });
